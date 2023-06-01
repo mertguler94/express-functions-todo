bfcdeda985601a0b2e00967a7f5578994c2d8009
mertguler94 <gulermert94@gmail.com>
2023-05-31 21:11:51 -0400

    implementing routes

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,12 +1,17 @@
 import { onRequest } from "firebase-functions/v2/https";
 import express from "express";
+import cors from "cors";
 import { type Request, Response } from "express";
+import todoRouter from "./routes/todos";
 
 const app = express();
+app.use(cors({ origin: true }));
 
 // ping
-app.get("/", (req: Request, res: Response) => {
+app.get("/ping", (req: Request, res: Response) => {
   res.status(200).send(`Server is running, the time is: ${Date.now()}`);
 });
+
+app.use("/todos", todoRouter);
 
 export const api = onRequest(app);
--- functions/src/routes/todos.ts
+++ functions/src/routes/todos.ts
@@ -1,31 +1,55 @@
 import express, { Request, Response } from "express";
+import db from "../config/db";
+import { TodoType } from "../models/Todo";
 // import { TodoType } from "../models/Todo";
-import admin from "firebase-admin";
-
-import serviceAccount from ("path/to/serviceAccountKey.json");
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
-});
 
 const router = express.Router();
 
 // get all todos
 
-router.get("/todos", (req: Request, res: Response) => {});
+router.get("/", async (req: Request, res: Response) => {
+  try {
+    const fsRes = await db.collection("/todos").get();
+    return res.status(200).json({ message: "OK", data: fsRes.docs });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ error: error, message: "Something went wrong." });
+  }
+});
 
 // get a todo by id
 
-router.get("/todos/:id", (req: Request, res: Response) => {});
+// router.get("/todos/:id", (req: Request, res: Response) => {});
 
 // post a todo
 
-router.post("/todos", (req: Request, res: Response) => {});
+router.post("/", async (req: Request, res: Response) => {
+  const todo = req.body as TodoType;
+  if (!todo) return res.status(404).send("Body cannot be found.");
+
+  const newTodo: TodoType = {
+    ...todo,
+    createdAt: Date.now(),
+    updatedAt: Date.now(),
+  };
+
+  try {
+    const fsRes = await db.collection("todos").doc().create(newTodo);
+    return res.status(200).json({ message: "OK", data: fsRes });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ error: error, message: "Something went wrong." });
+  }
+});
 
 // update a todo
 
-router.put("/todos/:id", (req: Request, res: Response) => {});
+// router.put("/todos/:id", (req: Request, res: Response) => {});
 
 // delete a todo
 
-router.delete("/todos/:id", (req: Request, res: Response) => {});
+// router.delete("/todos/:id", (req: Request, res: Response) => {});
+
+export default router;
